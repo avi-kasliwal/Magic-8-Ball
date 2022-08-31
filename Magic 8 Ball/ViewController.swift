5281e1450b2849941c913df64e74cf19cb9b23f5
Avi Kasliwal <avi.kasliwal04@gmail.com>
2022-08-31 15:23:16 +0530

    Mini Project 2 Linking Design With Code Programatically update the UI Arrays, Randomisation, Constants

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageViewBall: UIImageView!
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
-
+    @IBAction func askMeButton(_ sender: UIButton) {
+        imageViewBall.image = UIImage (named: "ball\(Int.random(in: 1...5))")
+        print ("Button Pressed!")
+    }
+    
 }
 
